de5c2d11589feef9da33a07f85be0363abe976de
Sotonye Orifama <sotorisfx@gmail.com>
2022-09-09 08:06:53 -0700

    alphABET added

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,20 +10,20 @@
 
 int main(void)
 {
-    int i, j;
+  int i, j;
 
-    i = 97;
-    j = 65;
-    while (i < 123)
-    {
-         putchar(i);
-         i++;
-    }
-    while (j < 91)
-    {
-         putchar(j);
-  	 j++;
-    }
-    putchar(10);
-    return (0);
+  i = 97;
+  j = 65;
+  while (i < 123)
+  {
+       putchar(i);
+       i++;
+  }
+  while (j < 91)
+  {
+       putchar(j);
+       j++;
+  }
+  putchar(10);
+  return (0);
 }
