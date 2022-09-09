379a76964f8da5421bda7fe0882f072ca2d1f538
Sotonye Orifama <sotorisfx@gmail.com>
2022-09-09 08:27:41 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,13 +16,13 @@ i = 97;
 j = 65;
 while (i < 123)
 {
-     putchar(i);
-     i++;
+putchar(i);
+i++;
 }
 while (j < 91)
 {
-     putchar(j);
-     j++;
+putchar(j);
+j++;
 }
 putchar(10);
 return (0);
