865e3be39838bf00bc8b432f6ec4db66e87ee9c6
Sotonye Orifama <sotorisfx@gmail.com>
2022-09-13 14:31:07 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @i: integer 1
+ * @j: integer 2
+ *
+ * Description: give two integers, returns sum
+ *
+ * Return: sum
+ */
+
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+
+	return (sum);
+}
